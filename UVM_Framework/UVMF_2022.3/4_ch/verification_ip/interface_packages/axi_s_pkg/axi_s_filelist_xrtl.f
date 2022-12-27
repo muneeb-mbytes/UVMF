555c2d31e8d6b928e096d97f5b06832dc133b0a3
vineethkumarv <vineethveepuri@mirafra.com>
2022-12-27 09:53:43 +0530

    tb_hvl_f edited

--- UVM_Framework/UVMF_2022.3/4_ch/verification_ip/interface_packages/axi_s_pkg/axi_s_filelist_xrtl.f
+++
@@ -1,3 +0,0 @@
-$UVMF_VIP_LIBRARY_HOME/interface_packages/axi_s_pkg/src/axi_s_if.sv
-$UVMF_VIP_LIBRARY_HOME/interface_packages/axi_s_pkg/src/axi_s_monitor_bfm.sv
-$UVMF_VIP_LIBRARY_HOME/interface_packages/axi_s_pkg/src/axi_s_driver_bfm.sv
